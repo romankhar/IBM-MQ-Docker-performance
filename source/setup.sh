73078e8795c2ca8c8e35a13024b859dbadaef223
Roman Kharkovski <kharkovski@gmail.com>
2017-01-06 10:48:53 -0500

    Update setup.sh

--- source/setup.sh
+++ source/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
 # DESCRIPTION:
-#	This script needs to be run once to prepare the host computer for tests. It is not necessary to run it again after reboots. 
-# All we do here is just build a base MQ Docker image for reuse in later stages of the project.
-# You only need to run it again if you need to upgrade to a new version of MQ or fixpack. This is usually done handful a times per year.
+#     This script needs to be run once to prepare the host computer for tests. It is not necessary to run it again after reboots. 
+#     All we do here is just build a base MQ Docker image for reuse in later stages of the project.
+#     You only need to run it again if you need to upgrade to a new version of MQ or fixpack. This is usually done handful a times per year.
 #
 # AUTHOR:   	
-# 	Roman Kharkovski (http://advantage.ibm.com/about)
+#     Roman Kharkovski (http://advantage.ibm.com/about)
 
 set -o nounset
 set -o errexit
